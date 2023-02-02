1b0a1fd3785aeeed2c128e9bfe05912535c12895
safeaf <transiencymail@gmail.com>
2023-02-02 11:13:34 -0700

    fixed users/comments

--- web/app/controllers/comments_controller.rb
+++ web/app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_listing
+  #before_action :set_listing
   before_action :set_comment, only: %i[ show edit update destroy]
 
   def new
