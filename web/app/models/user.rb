922f1106ab312cc727ab568fb9c14f94fd8c0399
safeaf <transiencymail@gmail.com>
2023-02-03 11:46:25 -0700

    changed views to use username rather than email

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
 end
