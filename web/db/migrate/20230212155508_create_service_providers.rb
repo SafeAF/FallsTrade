18143f5473b8d2c03d1014e6bb5014eafbede7a5
safeaf <transiencymail@gmail.com>
2023-02-12 09:18:42 -0700

    added providers to navbar

--- web/db/migrate/20230212155508_create_service_providers.rb
+++ web/db/migrate/20230212155508_create_service_providers.rb
@@ -1,13 +1,13 @@
 class CreateServiceProviders < ActiveRecord::Migration[7.0]
   def change
     create_table :service_providers do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :company
       t.string :bio
       t.string :phone
       t.string :email
       t.references :user, null: false, foreign_key: true
-
+      t.string :category
       t.timestamps
     end
   end
--- web/db/migrate/20230212155649_create_offerings.rb
+++ web/db/migrate/20230212155649_create_offerings.rb
@@ -1,9 +1,9 @@
 class CreateOfferings < ActiveRecord::Migration[7.0]
   def change
     create_table :offerings do |t|
-      t.string :title
+      t.string :title, null: false
       t.references :service_provider, null: false, foreign_key: true
-      t.text :description
+      t.text :description, null: false
       t.string :category
       t.string :phone
       t.string :email
